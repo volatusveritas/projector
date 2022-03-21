a37c476b6e9235c6e3bd332532082207fae488bc
Volatus <volatusCoder@outlook.com>
2022-03-20 22:34:20 -0300

    Create initial error types

--- error.py
+++ error.py
@@ -5,3 +5,25 @@ class ProjectorError(Exception):
 
     def __str__(self):
         return f"PJ{self.code}: {self.message}"
+
+
+class ProjectorTypeError(ProjectorError):
+    def __init__(self, context, type_name, extra=""):
+        super().__init__(
+            "001", f"Invalid value type '{type_name}' at {context}{extra}"
+        )
+
+
+class ProjectorValueError(ProjectorError):
+    def __init__(self, context):
+        super().__init__("002", f"Invalid value at {context}")
+
+
+class ProjectorDivisionByZeroError(ProjectorError):
+    def __init__(self, context):
+        super().__init__("003", f"Division by zero at {context}")
+
+
+class ProjectorUndefinedNameError(ProjectorError):
+    def __init__(self, context, name, extra=""):
+        super().__init__("004", f"Undefined name '{name}' at {context}{extra}")
