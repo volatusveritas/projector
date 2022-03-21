8eec601f74e9129a5f9c7195b6421ad314bc5202
lucasmartinscopas <lucas.martins@copastur.com.br>
2022-03-21 12:38:09 -0300

    Improve error messages

--- projector/error.py
+++ projector/error.py
@@ -1,29 +1,35 @@
 class ProjectorError(Exception):
-    def __init__(self, code="000", message="An unknown error occurred"):
+    def __init__(self,
+        code="000", message="An unknown error occurred",
+        context="unknown execution point"
+    ):
         self.code = code
         self.message = message
+        self.context = context
 
     def __str__(self):
-        return f"PJ{self.code}: {self.message}"
+        return f"PJ{self.code}: {self.message} at {self.context}"
 
 
 class ProjectorTypeError(ProjectorError):
     def __init__(self, context, type_name, extra=""):
         super().__init__(
-            "001", f"Invalid value type '{type_name}' at {context}{extra}"
+            "001", f"Invalid value type '{type_name}'", f"{context}{extra}"
         )
 
 
 class ProjectorValueError(ProjectorError):
-    def __init__(self, context):
-        super().__init__("002", f"Invalid value at {context}")
+    def __init__(self, context, extra=""):
+        super().__init__("002", f"Invalid value", f"{context}{extra}")
 
 
 class ProjectorDivisionByZeroError(ProjectorError):
-    def __init__(self, context):
-        super().__init__("003", f"Division by zero at {context}")
+    def __init__(self, context, extra=""):
+        super().__init__("003", f"Division by zero", f"{context}{extra}")
 
 
 class ProjectorUndefinedNameError(ProjectorError):
     def __init__(self, context, name, extra=""):
-        super().__init__("004", f"Undefined name '{name}' at {context}{extra}")
+        super().__init__(
+            "004", f"Undefined name '{name}'", f"{context}{extra}"
+        )
