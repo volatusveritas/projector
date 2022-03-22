185acefbccf2307aef31a64e06251146912508e4
Volatus <volatusCoder@outlook.com>
2022-03-21 23:01:35 -0300

    Reduce error message clutter

--- projector/exceptions.py
+++ projector/exceptions.py
@@ -1,52 +1,38 @@
 class ProjectorError(Exception):
-    def __init__(self,
-        code="000", message="An unknown error occurred",
-        context="unknown execution point"
-    ):
-        self.code = code
-        self.message = message
-        self.context = context
-
     def __str__(self):
-        return f"PJ{self.code}: {self.message} at {self.context}"
+        return "Unknown error"
 
 
 class ProjectorTypeError(ProjectorError):
-    def __init__(self, context, type, extra=""):
-        super().__init__(
-            "001", f"Invalid value type '{type}'", f"{context}{extra}"
-        )
+    def __str__(self):
+        return "Invalid type"
 
 
 class ProjectorValueError(ProjectorError):
-    def __init__(self, context, extra=""):
-        super().__init__("002", "Invalid value", f"{context}{extra}")
+    def __str__(self):
+        return "Invalid value"
 
 
 class ProjectorDivisionByZeroError(ProjectorError):
-    def __init__(self, context, extra=""):
-        super().__init__("003", "Division by zero", f"{context}{extra}")
+    def __str__(self):
+        return "Division by zero"
 
 
 class ProjectorUndefinedNameError(ProjectorError):
-    def __init__(self, context, name, extra=""):
-        super().__init__(
-            "004", f"Undefined name '{name}'", f"{context}{extra}"
-        )
+    def __str__(self):
+        return "Undefined name"
 
 
 class ProjectorOperatorAbsentError(ProjectorError):
-    def __init__(self, extra=""):
-        super().__init__("010", "Expected operator", f"parsing{extra}")
+    def __str__(self):
+        return "Operator absent"
 
 
 class ProjectorUnmatchedParenthesesError(ProjectorError):
-    def __init__(self, extra=""):
-        super().__init__("020", "Unmatched parentheses", f"tokenization{extra}")
+    def __str__(self):
+        return "Unmatched parentheses"
 
 
 class ProjectorInvalidSymbolError(ProjectorError):
-    def __init__(self, symbol, extra=""):
-        super().__init__(
-            "021", f"Invalid symbol '{symbol}'", f"tokenization{extra}"
-        )
+    def __str__(self):
+        return "Invalid symbol"
