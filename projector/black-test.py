b7b5e2e64e05e29c783dc99cafcc660b4a363070
Volatus <volatusCoder@outlook.com>
2022-04-03 00:11:59 -0300

    Black workflow test #0

---
+++ projector/black-test.py
@@ -0,0 +1,9 @@
+def big_bananas_little_coconuts_humongous_strawberries(first_banana, second_coconut, third_strawberry):
+    my_nice_array=[    4,3 ,2,5 ,8,'banana'    ]
+    for object in my_nice_array:print( object )
+    if my_nice_array[0] == my_nice_array[2] and my_nice_array[0] != my_nice_array[3] or my_nice_array[4] == my_nice_array[5] and True:
+        print(    "Carlos"    )
+    return my_nice_array[0] == 3 or first_banana == 2 or second_coconut == "coconut" and third_strawberry == True
+
+
+print(big_bananas_little_coconuts_humongous_strawberries(3, "cake", True))
